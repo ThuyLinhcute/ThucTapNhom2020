f1698b8671ff43d1871f89d5763808314ac742d1
Jenny Fai <59854176+ThuyLinhcute@users.noreply.github.com>
2020-08-22 09:36:03 +0700

    Frame_QLHS

---
+++ TTNhom_QuanLyHocSinh/TTNhom_QuanLyHocSinh/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TTNhom_QuanLyHocSinh.Views;
+using TTNhom_QuanLyHocSinh.QLHocSinh;
+using TTNhom_QuanLyHocSinh.Object;
+using TTNhom_QuanLyHocSinh.QLGiaoVien;
+
+namespace TTNhom_QuanLyHocSinh
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+     
+        [STAThread]
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+          
+          //  Application.Run(lg);
+        }
+    }
+}
