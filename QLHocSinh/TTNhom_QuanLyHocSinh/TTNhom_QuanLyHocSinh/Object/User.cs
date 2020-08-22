15e98715e4f2d02228c9c8ef88df17cf9884b6e8
Jenny Fai <59854176+ThuyLinhcute@users.noreply.github.com>
2020-08-22 10:19:31 +0700

    QuanLyNguoiDung_L

---
+++ QLHocSinh/TTNhom_QuanLyHocSinh/TTNhom_QuanLyHocSinh/Object/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTNhom_QuanLyHocSinh.Object
+{
+    public class User
+    {
+        string username;
+        string password;
+        public enum UserFields
+        {
+            tendangnhap,
+            matkhau,
+        }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
