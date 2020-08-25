9a68595a2c9fcee99c9e65b0336c554ad518ddda
duyen050799 <phanthimyduyen5799@gmail.com>
2020-08-25 18:47:55 +0700

    Duyen_GiaoVien

--- QLHocSinh/TTNhom_QuanLyHocSinh/TTNhom_QuanLyHocSinh/Program.cs
+++ QLHocSinh/TTNhom_QuanLyHocSinh/TTNhom_QuanLyHocSinh/Program.cs
@@ -6,36 +6,43 @@ using System.Windows.Forms;
 using TTNhom_QuanLyHocSinh.Views;
 using TTNhom_QuanLyHocSinh.QLHocSinh;
 using TTNhom_QuanLyHocSinh.Object;
+using TTNhom_QuanLyHocSinh.QLGiaoVien;
 
 namespace TTNhom_QuanLyHocSinh
 {
-     static class Program
-     {
-          /// <summary>
-          /// The main entry point for the application.
-          /// </summary>
-          public static QLHS qLHS;
-          public static frmLogin lg;
-          public static QuanLyChung quanLyChung;
-          public static int opt;
-          public static HocSinhSql hocSinhSql;
-          public static HocSinh hs;
-         
-          public static string mamonhoc;
-          public static UserSql userSql;
-          [STAThread]
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        public static QLHS qLHS;
+        public static frmLogin lg;
+        public static QuanLyChung quanLyChung;
+        public static int opt;
+        public static HocSinhSql hocSinhSql;
+        public static HocSinh hs;
+        public static GiaoVienSql giaoVienSql;
+        public static GiaoVien gv;
+        public static QLGV qLGV;
+       
+        public static string mamonhoc;
+        public static UserSql userSql;
+        [STAThread]
 
-          static void Main()
-          {
-               Application.EnableVisualStyles();
-               Application.SetCompatibleTextRenderingDefault(false);
-               qLHS = new QLHS();
-               lg = new frmLogin();
-               quanLyChung = new QuanLyChung();
-               hocSinhSql = new HocSinhSql();
-               hs = new HocSinh();
-               userSql = new UserSql();
-               Application.Run(lg);
-          }
-     }
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            qLHS = new QLHS();
+            lg = new frmLogin();
+            quanLyChung = new QuanLyChung();
+            hocSinhSql = new HocSinhSql();
+            hs = new HocSinh();
+            giaoVienSql = new GiaoVienSql();
+            gv = new GiaoVien();
+            qLGV = new QLGV();
+            userSql = new UserSql();
+            Application.Run(lg);
+        }
+    }
 }
