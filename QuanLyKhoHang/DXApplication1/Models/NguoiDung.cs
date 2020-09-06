c7d15152f1db7d68f96f89ce62de344d8577c96f
MoNguyen127 <70472268+MoNguyen127@users.noreply.github.com>
2020-09-06 23:48:14 +0700

    mo

---
+++ QuanLyKhoHang/DXApplication1/Models/NguoiDung.cs
@@ -0,0 +1,55 @@
+﻿namespace DXApplication1.Models
+{
+    class NguoiDung : DataLayer.DataAccess
+    {
+        string _name;
+        string _pass;
+        ThongTinNguoiDung _thongTinNguoiDung;
+
+        public NguoiDung()
+        {
+
+        }
+        public enum UserFields
+        {
+            MaDangNhapNguoiDung,
+            MatKhau,
+            ThongTinNguoiDung
+        }
+        public string MaDangNhapNguoiDung
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                }
+            }
+        }
+        public string MatKhau
+        {
+            get { return _pass; }
+            set
+            {
+                if (_pass != value)
+                {
+                    _pass = value;
+                }
+            }
+        }
+
+        public ThongTinNguoiDung ThongTinNguoiDung
+        {
+            get { return _thongTinNguoiDung; }
+            set
+            {
+                if (_thongTinNguoiDung != value)
+                {
+                    _thongTinNguoiDung = value;
+                }
+            }
+        }
+
+    }
+}
