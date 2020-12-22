1b320b76be33e9f60ca5ad0d1a6873fff7282393
duyen050799 <phanthimyduyen5799@gmail.com>
2020-12-22 09:19:41 +0700

    duyen_baocaothongke

--- QuanLySieuThi/QuanLySieuThi-master/BanHangSieuTHi/frmCheckOut.cs
+++ QuanLySieuThi/QuanLySieuThi-master/BanHangSieuTHi/frmCheckOut.cs
@@ -41,69 +41,70 @@ namespace BanHangSieuTHi
         }
 
         private void btnTT_Click(object sender, EventArgs e)
-        {           
-        //     Random random = new Random(); 
-        //     int SoHDX = random.Next(1000, 1999);
-            
-        //    while(KiemTraTrungLap(SoHDX.ToString()))
-        //     {
-        //         SoHDX = random.Next(1000, 1999);
-        //     }
+        {
+            Random random = new Random();
+            int SoHDX = random.Next(1000, 1999);
 
-        //     DialogResult result;
-        //     result = MessageBox.Show("BẠN CÓ CHẮC CHẮN MUỐN THANH TOÁN ĐƠN HÀNG NÀY", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-        //     if (result == DialogResult.Yes)
-        //     {
+            while (KiemTraTrungLap(SoHDX.ToString()))
+            {
+                SoHDX = random.Next(1000, 1999);
+            }
 
-        //         //sqlQuery truyVan = new sqlQuery();
-        //         //truyVan.ExecuteNoneQuery("Insert into HOADONXUAT(SoHDX,NgayXuat,ThanhTienXuat,MaNV,MaKH) values('"+SoHDX.ToString()+"','"+DateTime.Now+"','"+TongTien.ToString()+"','"+MaNV+"','"+DBNull.Value+"')");
-        //         string connectionString = @"Data Source=DESKTOP-09FMHR8;Initial Catalog=QLKhoHang;Integrated Security=True";
-        //         SqlConnection con = new SqlConnection(connectionString);
-        //         con.Open();
-        //         string str = "insert into HOADONXUAT values(@SoHDX,@NgayXuat,@ThanhTienXuat,@MaNV,@MaKH)";
-        //         SqlCommand cmd = new SqlCommand(str, con);
-        //         cmd.CommandType = CommandType.Text;
-        //         cmd.Parameters.AddWithValue("@SoHDX", SoHDX.ToString());
-        //         cmd.Parameters.AddWithValue("@NgayXuat", DateTime.Now);
-        //         cmd.Parameters.AddWithValue("@ThanhTienXuat", TongTien.ToString());
-        //         cmd.Parameters.AddWithValue("@MaNV", MaNV);
-        //         if (MaKH == "")
-        //         {
-        //             cmd.Parameters.AddWithValue("@MaKH", DBNull.Value); }
-        //         else
-        //         {
-        //             cmd.Parameters.AddWithValue("@MaKH", MaKH);
-        //         }
-        //         cmd.ExecuteNonQuery(); //thực thi            
+            DialogResult result;
+            result = MessageBox.Show("BẠN CÓ CHẮC CHẮN MUỐN THANH TOÁN ĐƠN HÀNG NÀY", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
 
-        //         if (con.State == ConnectionState.Open)
-        //             con.Close();
+                //sqlQuery truyVan = new sqlQuery();
+                //truyVan.ExecuteNoneQuery("Insert into HOADONXUAT(SoHDX,NgayXuat,ThanhTienXuat,MaNV,MaKH) values('"+SoHDX.ToString()+"','"+DateTime.Now+"','"+TongTien.ToString()+"','"+MaNV+"','"+DBNull.Value+"')");
+                string connectionString = @"Data Source=DESKTOP-09FMHR8;Initial Catalog=QLKhoHang;Integrated Security=True";
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                string str = "insert into HOADONXUAT values(@SoHDX,@NgayXuat,@ThanhTienXuat,@MaNV,@MaKH)";
+                SqlCommand cmd = new SqlCommand(str, con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@SoHDX", SoHDX.ToString());
+                cmd.Parameters.AddWithValue("@NgayXuat", DateTime.Now);
+                cmd.Parameters.AddWithValue("@ThanhTienXuat", TongTien.ToString());
+                cmd.Parameters.AddWithValue("@MaNV", MaNV);
+                if (MaKH == "")
+                {
+                    cmd.Parameters.AddWithValue("@MaKH", DBNull.Value);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@MaKH", MaKH);
+                }
+                cmd.ExecuteNonQuery(); //thực thi            
 
-        //         MessageBox.Show("THANH TOÁN THÀNH CÔNG", "THÔNG BÁO");
-        //         this.Close();
-        //     }
-           
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+
+                MessageBox.Show("THANH TOÁN THÀNH CÔNG", "THÔNG BÁO");
+                this.Close();
+            }
+
         }
 
         private bool KiemTraTrungLap(string x)
         {
-            // int temp=0;
-            // sqlQuery truyVan = new sqlQuery();
-            // DataTable td = truyVan.LayDuLieu("select SoHDX from HOADONXUAT ");
-            // for(int i = 0; i < td.Rows.Count; i++)
-            // {
-            //     if (x == td.Rows[i][0].ToString())
-            //     {
-            //         temp = 1;
-            //     }
-            //     else { temp = 0; }
+            int temp = 0;
+            sqlQuery truyVan = new sqlQuery();
+            DataTable td = truyVan.LayDuLieu("select SoHDX from HOADONXUAT ");
+            for (int i = 0; i < td.Rows.Count; i++)
+            {
+                if (x == td.Rows[i][0].ToString())
+                {
+                    temp = 1;
+                }
+                else { temp = 0; }
 
-            // }
-            // if (temp == 0)
-            // {
-            //     return false;
-            // }
-            // else { return true; }
+            }
+            if (temp == 0)
+            {
+                return false;
+            }
+            else { return true; }
         }
     }
 }
