4177717ade14b2bd1514f0c03aaa093018c2039f
Jenny Fai <59854176+ThuyLinhcute@users.noreply.github.com>
2020-12-22 14:48:11 +0700

    ThemUser_Lien

--- QuanLySieuThi/QuanLySieuThi-master/BanHangSieuTHi/frmThemUser.cs
+++ QuanLySieuThi/QuanLySieuThi-master/BanHangSieuTHi/frmThemUser.cs
@@ -20,31 +20,31 @@ namespace BanHangSieuTHi
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            // try
-            // {
-            //     if (tbMatKhau.Text == tbMatKhau2.Text)
-            //     {
-            //         DialogResult result;
-            //         result = MessageBox.Show("BẠN CÓ MUỐN XÓA NGƯỜI DÙNG NÀY KHÔNG?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            //         if (result == DialogResult.Yes)
-            //         {
-            //             string[] name = { "@TenDangNhap", "@TenNguoiDung", "@MatKhau", "@HinhAnh", "@QuyenTruyCap" };
-            //             string[] value = { tbTenDangNhap.Text, tbTenNguoiDung.Text, tbMatKhau.Text, tbImage.Text, cbQuyen.SelectedItem.ToString() };
-            //             sqlQuery sql = new sqlQuery();
-            //             sql.update("ADD_USER", name, value, 5);
-            //             MessageBox.Show("THÊM NGƯỜI DÙNG THÀNH CÔNG !", "");
-            //             this.Close();
-            //         }
-            //     }
-            //     else
-            //     {
-            //         MessageBox.Show("Kiểm tra lại phần xác nhận mật khẩu và mật khẩu ko giống nhau!", "THÔNG BÁO");
-            //     }
-            // }
-            // catch (Exception)
-            // {
-            //     MessageBox.Show("Bạn đã nhập đủ các trường cần thiết chưa??", "Warning");
-            // }
+            try
+            {
+                if (tbMatKhau.Text == tbMatKhau2.Text)
+                {
+                    DialogResult result;
+                    result = MessageBox.Show("BẠN CÓ MUỐN XÓA NGƯỜI DÙNG NÀY KHÔNG?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        string[] name = { "@TenDangNhap", "@TenNguoiDung", "@MatKhau", "@HinhAnh", "@QuyenTruyCap" };
+                        string[] value = { tbTenDangNhap.Text, tbTenNguoiDung.Text, tbMatKhau.Text, tbImage.Text, cbQuyen.SelectedItem.ToString() };
+                        sqlQuery sql = new sqlQuery();
+                        sql.update("ADD_USER", name, value, 5);
+                        MessageBox.Show("THÊM NGƯỜI DÙNG THÀNH CÔNG !", "");
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Kiểm tra lại phần xác nhận mật khẩu và mật khẩu ko giống nhau!", "THÔNG BÁO");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bạn đã nhập đủ các trường cần thiết chưa??", "Warning");
+            }
         }
 
         private void btnClose_Click(object sender, EventArgs e)
