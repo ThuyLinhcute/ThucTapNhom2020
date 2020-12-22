fd252e480803664610d6f039fa2faf2dd9ee055a
JackG55 <jacksparrow551999@gmail.com>
2020-12-22 10:42:50 +0700

    tạo form khách hàng

--- QuanLyKhoHang/DXApplication1/Views/Form_KH.cs
+++ QuanLyKhoHang/DXApplication1/Views/Form_KH.cs
@@ -20,13 +20,13 @@ namespace DXApplication1.Views
 
         private void Form_KH_Load(object sender, EventArgs e)
         {
-            //DataTable dt = DataAccess.Query("select * from Khachhang");
-            //dataGridView1.DataSource = dt;
+            DataTable dt = DataAccess.Query("select * from Khachhang");
+            dataGridView1.DataSource = dt;
         }
         private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
         {
-            //lblID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
-            //lblTen.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            lblID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            lblTen.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
         }
 
         #region Push Information
@@ -36,26 +36,26 @@ namespace DXApplication1.Views
         public GetData Getmydata;
         private void btn_Click(object sender, EventArgs e)
         {
-            //Button btn = (Button)sender;
-            //if (btn.Text == "OK")
-            //{
-            //    if (lblID.Text == "")
-            //        MessageBox.Show("Chọn một nhà khách hàng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            //    else
-            //    {
-            //        Getmydata(lblID.Text, lblTen.Text);
-            //        this.Hide();
-            //    }
-            //}
-            //else
-            //    Close();
+            Button btn = (Button)sender;
+            if (btn.Text == "OK")
+            {
+                if (lblID.Text == "")
+                    MessageBox.Show("Chọn một nhà khách hàng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    Getmydata(lblID.Text, lblTen.Text);
+                    this.Hide();
+                }
+            }
+            else
+                Close();
         }
         #endregion
 
         private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
         {
-            //lblID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
-            //lblTen.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            lblID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            lblTen.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
         }
     }
 }
